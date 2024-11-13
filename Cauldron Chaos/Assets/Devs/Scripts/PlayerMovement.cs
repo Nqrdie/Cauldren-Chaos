918d5b91ab28895fcf00bf3d9da18881d7cfe336
Nqrdie <rrvanleeuwen@kpnmail.nl>
2024-11-13 15:34:58 +0100

    Input, PlayerMovement, PlayerPush

---
+++ Cauldron Chaos/Assets/Devs/Scripts/PlayerMovement.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CauldronChaos input;
+
+    private Vector3 moveVector = Vector3.zero;
+
+    private Rigidbody rb;
+    private bool inRange;
+    private GameObject enemy;
+
+    
+
+    [SerializeField] float speed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float pushStrength;
+
+    private void Awake()
+    {
+       input = new CauldronChaos();
+    }
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Move.performed += Move_performed;
+        input.Player.Move.canceled += Move_canceled;
+        input.Player.Fire.performed += Push_Performed;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Move.performed -= Move_performed;
+        input.Player.Move.canceled -= Move_canceled;
+        input.Player.Fire.performed -= Push_Performed;
+    }
+
+
+    public void Push_Performed(InputAction.CallbackContext value)
+    {
+        if (inRange && enemy != null)
+        {
+            Debug.Log(enemy);
+
+            if (enemy.TryGetComponent<Rigidbody>(out Rigidbody enemyRb))
+            {
+                enemyRb.AddForce(transform.forward * pushStrength, ForceMode.Impulse);
+                enemy.transform.LookAt(transform.position);
+            }
+            //enemy.transform.position += transform.forward * 2;
+        }
+    }
+
+    public void Move_canceled(InputAction.CallbackContext value)
+    {
+        moveVector = Vector3.zero;
+    }
+
+    public void Move_performed(InputAction.CallbackContext value)
+    {
+        moveVector = new Vector3(value.ReadValue<Vector3>().x, 0, value.ReadValue<Vector3>().z);
+        moveVector.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        print(moveVector);
+        rb.velocity = moveVector * speed * Time.fixedDeltaTime * 100;
+    }
+
+    private void Update()
+    {
+        if (moveVector != Vector3.zero)
+        {
+            //transform.forward = moveVector;
+
+            Quaternion toRotation = Quaternion.LookRotation(moveVector, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {   
+            if (other = other.gameObject.GetComponent<CapsuleCollider>())
+            {
+                inRange = true;
+                enemy = other.gameObject;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inRange = false;
+    }
+}

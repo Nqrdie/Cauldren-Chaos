e4a88dc31c418e399d22acdb4239ad4fc72e990f
Nqrdie <rrvanleeuwen@kpnmail.nl>
2024-11-15 12:03:46 +0100

    Change prefabs

--- Cauldron Chaos/Assets/Devs/Scripts/FallingObject.cs
+++ Cauldron Chaos/Assets/Devs/Scripts/FallingObject.cs
@@ -1,18 +1,15 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class FallingObject : MonoBehaviour
 {
+    [SerializeField] private GameObject childObject;
+
     [SerializeField] private float timeToFade;
     private Color objColor;
     private Color alphaColor;
     private Material objMaterial;
-    [SerializeField] private GameObject childObject;
     [SerializeField] float shadowSize;
-    private bool collided = false;
-
     private Vector3 size;
     private Vector3 originalSize;
     private Vector3 childSize;
@@ -28,29 +25,39 @@ public class FallingObject : MonoBehaviour
         size = Vector3.zero;
         originalSize = gameObject.transform.localScale;
         childSize = gameObject.transform.localScale / 4;
-}
+    }
+
+    private void Update()
+    {
+        UpdateShadow();
+    }
+
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+
         Destroy(childObject);
         StartCoroutine(Despawn());
-        collided = true;
     }
-    private void Update()
+
+    private void UpdateShadow()
     {
         if (childObject != null && size != originalSize)
         {
             size = new Vector3(
-                childSize.x += shadowSize * Time.deltaTime, 
-                childSize.y += shadowSize * Time.deltaTime, 
+                childSize.x += shadowSize * Time.deltaTime,
+                childSize.y += shadowSize * Time.deltaTime,
                 childSize.z += shadowSize * Time.deltaTime
                 );
 
             childObject.transform.localScale = size;
         }
-
-        if (size == originalSize) { print("yes"); }
     }
+
     private IEnumerator Despawn()
     {
         float elapsedTime = 0f;

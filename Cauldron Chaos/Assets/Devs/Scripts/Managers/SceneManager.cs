1e52fa392853eec9055113032c5b31f75e06d680
Izzyy-y <230048@student.glu.nl>
2024-11-25 13:46:04 +0100

    menu navigation

--- Cauldron Chaos/Assets/Devs/Scripts/Managers/SceneManager.cs
+++ Cauldron Chaos/Assets/Devs/Scripts/Managers/SceneManager.cs
@@ -5,15 +5,45 @@ using UnityEngine;
 
 public class SceneChanger : MonoBehaviour
 {
-
+    private const string StartSceneName = "Start Scene";
+    private const string CharacterSelectSceneName = "Character Selection Scene";
+    private const string SettingsSceneName = "Settings Scene";
+    private const string CreditsSceneName = "Credits Scene";
     private const string MainSceneName = "Main Scene";
-    private const string EndSceneName = "EndScene";
+    private const string EndSceneName = "End Scene";
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(StartSceneName);
+    }
+
+    public void LoadCharacterSelectScene()
+    {
+        SceneManager.LoadScene(CharacterSelectSceneName);
+    }
+
+    public void LoadSettingsScene()
+    {
+        SceneManager.LoadScene(SettingsSceneName);
+    }
+    
+    public void LoadCreditsScene()
+    {
+        SceneManager.LoadScene(CreditsSceneName);
+    }
+
     public void LoadMainScene()
     {
         SceneManager.LoadScene(MainSceneName);
     }
+
     public void LoadEndScene()
     {
         SceneManager.LoadScene(EndSceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
